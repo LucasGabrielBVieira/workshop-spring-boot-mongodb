33d6bd77c25fc97226e37a2e96c009d108467ffa
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-08-13 09:36:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasgbvieira/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasgbvieira/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasgbvieira.workshopmongo.services;
 
 import com.lucasgbvieira.workshopmongo.domain.User;
+import com.lucasgbvieira.workshopmongo.dto.UserDTO;
 import com.lucasgbvieira.workshopmongo.repository.UserRepository;
 import com.lucasgbvieira.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,15 @@ public class UserService {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDto) {
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+    }
+
+
 
 }
